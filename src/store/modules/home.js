3e6f2ed6aeefd017daa0aa8d72f0f046f15ff5cb
huanggaofeng <huanggaofeng@linewell.com>
2023-11-29 10:52:15 +0800

    home首页高性价比数据请求

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,13 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { getHomeGoodPriceData } from '@/services'
+
+// 异步actions  网络请求
+// 高性价比
+export const fetchHomeDataAction = createAsyncThunk('fetchdata', async (payload) => {
+  const res = await getHomeGoodPriceData()
+  return res
+})
 
 const homeSlice = createSlice({
   name: 'home',
   initialState: {
-    productList: []
+    goodPriceInfo: {}
   },
   reducers: {
-
+    changeGoodPriceInfo(state, {payload}) {
+      state.goodPriceInfo = payload
+    }
+  },
+  extraReducers: {
+    [fetchHomeDataAction.fulfilled](state, {payload}) {
+      state.goodPriceInfo = payload
+      console.log("state.goodPriceInfo", state.goodPriceInfo);
+    }
   }
 })
+
+export const { changeGoodPriceInfo } = homeSlice.actions
 
 export default homeSlice.reducer
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,11 +1,37 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+
+import { HomeWrapper } from './style'
+import HomeBanner from './c-cpns/home-banner'
+import { fetchHomeDataAction } from '@/store/modules/home'
 
 
 const Home = memo(() => {
+  // 派发异步事件: 发送网络请求
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchHomeDataAction())
+  }, [dispatch])
+
+  // redux获取数据
+  const { goodPriceInfo } =  useSelector((state) => ({
+    goodPriceInfo: state.home.goodPriceInfo
+  }), shallowEqual)
+
   return (
-    <div>
-     <h2>Home Page</h2>
-    </div>
+    <HomeWrapper>
+      <HomeBanner />
+      <div className='content'>
+        <h2>{goodPriceInfo.title}</h2>
+        <ul>
+          {
+          goodPriceInfo.list && goodPriceInfo.list.map(item => {
+            return <li key={item.id}>{item.name}</li>
+          })
+          }
+        </ul>
+      </div>
+    </HomeWrapper>
   )
 })
 
